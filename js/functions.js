2ae5f0a33d5bcf9d27f2e3c3e719f793e1145772
Raúl De Zamacona <raul@zamacona.me>
2015-08-25 16:37:26 -0500

    validación, colores

--- js/functions.js
+++ js/functions.js
@@ -143,6 +143,27 @@ function changeBackgroundImage(element){
 	$(element).css('background-image', 'url(images/hero-home-'+total+'.jpg)');
 }
 
+function onlyNumbers( e ) {
+	// Allow: backspace, delete, tab, escape, enter and .
+	//alert( e.keyCode );
+	if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190 ]) !== -1 ||
+		 // Allow: Ctrl+A
+		(e.keyCode == 65 && e.ctrlKey === true) ||
+		 // Allow: Ctrl+C
+		(e.keyCode == 67 && e.ctrlKey === true) ||
+		 // Allow: Ctrl+X
+		(e.keyCode == 88 && e.ctrlKey === true) ||
+		 // Allow: home, end, left, right
+		(e.keyCode >= 35 && e.keyCode <= 39)) {
+			 // let it happen, don't do anything
+			 return false;
+	}
+	// Ensure that it is a number and stop the keypress
+	if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+		return true;
+	}
+}// onlyNumbers
+
 
 
 /*------------------------------------*\
